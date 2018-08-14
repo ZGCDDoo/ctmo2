4896ceed4f8dea8bedcd4409379e8d2b42f25a2c
Charles-David Hebert <hebertcharlesdavid@outlook.com>
2018-08-14 13:59:41 -0400

    Moved H0TriangleTests to ABC_H0Tests

--- test/ABC_H0Tests.cpp
+++ test/ABC_H0Tests.cpp
@@ -1,16 +1,35 @@
 
-
 #include <gtest/gtest.h>
 #include "../src/Includes/Models/ABC_H0.hpp"
 #include "TestTools.hpp"
 
 const double DELTA = 1e-7;
-const double DELTA_SMALL = 1e-11;
+// const double DELTA_SMALL = 1e-11;
 const size_t Nc = 4;
 const size_t Nx = 2;
 const size_t Ny = 2;
+const size_t NOrb = 2;
 
-TEST(ModelTriangle2DTest, Init)
+Json BuildJson()
+{
+    Json tJson = R"(
+    {   "NOrb": 2,
+        "tParameters": 
+            {"00": 
+                {"tIntra": 0.0, "tx": -1.0, "ty": -1.0, "tx=y": -0.40, "tx=-y": 0.0, "t2x" : 0.0, "t2y": 0.0},
+            "01":
+                {"tIntra": 0.0, "tx": 0.0, "ty": 0.0, "tx=y": 0.0, "tx=-y": 0.0, "t2x" : 0.0, "t2y": 0.0},
+            "11":
+                {"tIntra": 0.0, "tx": -1.0, "ty": -1.0, "tx=y": -0.40, "tx=-y": 0.0, "t2x" : 0.0, "t2y": 0.0}
+
+            }
+    }
+    )"_json;
+
+    return tJson;
+}
+
+TEST(ABC_H0_Tests, Init)
 {
 
     Json tJson = R"(
@@ -31,7 +50,7 @@ TEST(ModelTriangle2DTest, Init)
     Json jj = tJson["tParameters"];
     assert(jj.size() == 3);
 
-    Models::ABC_H0<Nx, Nx> h0(tJson);
+    Models::ABC_H0<Nx, Ny> h0(tJson);
 
     std::vector<double> tIntraVec = {0.0, -100.0, 0.201};
     std::vector<double> txVec = {1.0, 1.10, -1.01};
@@ -50,6 +69,69 @@ TEST(ModelTriangle2DTest, Init)
         ASSERT_DOUBLE_EQ(h0.tx_yVec().at(ii), tx_yVec.at(ii));
         ASSERT_DOUBLE_EQ(h0.t2xVec().at(ii), t2xVec.at(ii));
         ASSERT_DOUBLE_EQ(h0.t2yVec().at(ii), t2yVec.at(ii));
+    }
+}
+
+TEST(ABC_H0_Tests, Init2)
+{
+    Json tJson = BuildJson();
+
+    Models::ABC_H0<Nx, Ny> h0(tJson);
+
+    ASSERT_EQ(h0.RSites().size(), Nc);
+    ASSERT_EQ(h0.KWaveVectors().size(), Nc);
+    ASSERT_EQ(h0(0.0, 0.0).n_cols, Nc * NOrb);
+
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[0][0], 0.0);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[0][1], 0.0);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[1][0], M_PI);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[1][1], 0.0);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[2][0], 0.0);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[2][1], M_PI);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[3][0], M_PI);
+    ASSERT_DOUBLE_EQ(h0.KWaveVectors()[3][1], M_PI);
+
+    ASSERT_DOUBLE_EQ(h0.RSites()[0][0], 0.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[0][1], 0.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[1][0], 1.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[1][1], 0.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[2][0], 0.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[2][1], 1.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[3][0], 1.0);
+    ASSERT_DOUBLE_EQ(h0.RSites()[3][1], 1.0);
+
+    ASSERT_DOUBLE_EQ(-4.6847345710802575, h0.Eps0k(-0.1, 0.3, 0));
+}
+
+TEST(ABC_H0_Tests, Hopping)
+{
+
+    Json tJson = BuildJson();
+
+    Models::ABC_H0<Nx, Nx> h0(tJson);
+
+    ClusterMatrixCD_t GoodHoppingKTilde(Nc * NOrb, Nc * NOrb);
+    GoodHoppingKTilde.zeros();
+    ClusterMatrixCD_t diagonal_part = {
+        {cd_t(0.0, 0.0), cd_t(-1.98006658, 0.19866933), cd_t(-1.82533561, -0.56464247), cd_t(-0.76842440, -0.15576734)},
+        {cd_t(-1.98006658, -0.19866933), cd_t(0.0, 0.0), cd_t(-0.72216088, -0.30532472), cd_t(-1.82533561, -0.56464247)},
+        {cd_t(-1.82533561, 0.56464247), cd_t(-0.72216088, 0.30532472), cd_t(0.0, 0.0), cd_t(-1.98006658, 0.19866933)},
+        {cd_t(-0.76842440, 0.15576734), cd_t(-1.82533561, 0.56464247), cd_t(-1.98006658, -0.19866933), cd_t(0.0, 0.0)}};
+
+    ClusterMatrixCD_t off_diagonal = {};
+    GoodHoppingKTilde.submat(0, 0, Nc - 1, Nc - 1) = diagonal_part;
+
+    GoodHoppingKTilde.submat(Nc, Nc, NOrb * Nc - 1, NOrb * Nc - 1) = diagonal_part;
+
+    ClusterMatrixCD_t hopping = h0(0.1, -0.3);
+    for (size_t i = 0; i < Nc * NOrb; i++)
+    {
+        for (size_t j = 0; j < Nc * NOrb; j++)
+        {
+            //std::cout << "i ,j " << i << " " << j << std::endl;
+            ASSERT_NEAR(hopping(i, j).real(), GoodHoppingKTilde(i, j).real(), DELTA);
+            ASSERT_NEAR(hopping(i, j).imag(), GoodHoppingKTilde(i, j).imag(), DELTA);
+        }
     }
 }
 
