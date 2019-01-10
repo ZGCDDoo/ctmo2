e2714f709f499dfb3dc993e88e2ccbc5b5c1974b
Charles-David Hebert <hebertcharlesdavid@outlook.com>
2019-01-09 21:28:01 -0500

    Key is now UInt64_t for all pertinent object in verticesSimple.
    For now, UInt64_t = size_t, but later, for vertex calculations, could change.

--- src/Includes/Utilities/VerticesSimple.hpp
+++ src/Includes/Utilities/VerticesSimple.hpp
@@ -175,22 +175,19 @@ class Vertices
         if (spin == FermionSpin_t::Up)
         {
             //Find in order the vertexParts corresponding to the same vertex
-            for (size_t ii = 0; ii < indexPartUpVec_.size(); ii++)
+
+            auto iitt = std::find(indexPartUpVec_.begin(), indexPartUpVec_.end(), key);
+            if (iitt != indexPartUpVec_.end())
             {
-                if (key == indexPartUpVec_.at(ii))
-                {
-                    indices.push_back(ii);
-                }
+                indices.push_back(std::distance(indexPartUpVec_.begin(), iitt));
             }
         }
         else
         {
-            for (size_t ii = 0; ii < indexPartDownVec_.size(); ii++)
+            auto iitt = std::find(indexPartDownVec_.begin(), indexPartDownVec_.end(), key);
+            if (iitt != indexPartDownVec_.end())
             {
-                if (key == indexPartDownVec_.at(ii))
-                {
-                    indices.push_back(ii);
-                }
+                indices.push_back(std::distance(indexPartDownVec_.begin(), iitt));
             }
         }
 
@@ -203,23 +200,6 @@ class Vertices
         AssertSizes();
 
         return verticesKeysVec_.at(pp);
-    }
-
-    std::vector<size_t> GetIndicesSpins(const size_t &pp, const FermionSpin_t &spin) const
-    {
-        const UInt64_t vertexKey = verticesKeysVec_.at(pp);
-        const VertexPart x = data_.at(pp).vStart();
-        const VertexPart y = data_.at(pp).vEnd();
-        std::vector<size_t> indices;
-
-        indices.push_back(GetKeyIndex(vertexKey, spin));
-
-        if (x.spin() == y.spin())
-        {
-            indices.push_back(GetKeyIndex(vertexKey + 1, spin));
-        }
-
-        return indices;
     }
 
     void SaveConfig(const std::string &fname)
@@ -301,9 +281,9 @@ class Vertices
     std::vector<Vertex> data_;
     std::vector<VertexPart> vPartUpVec_;
     std::vector<VertexPart> vPartDownVec_;
-    std::vector<size_t> indexPartUpVec_;
-    std::vector<size_t> indexPartDownVec_;  // Ex: The row and col 0 of Ndown_ is associtated to the vertexPart of the vertex given by the id of indexPartDownVec_.at(0)
-    std::vector<UInt64_t> verticesKeysVec_; // Each vertex has a unqique key identifying it
+    std::vector<UInt64_t> indexPartUpVec_;
+    std::vector<UInt64_t> indexPartDownVec_; // Ex: The row and col 0 of Ndown_ is associtated to the vertexPart of the vertex given by the id of indexPartDownVec_.at(0)
+    std::vector<UInt64_t> verticesKeysVec_;  // Each vertex has a unqique key identifying it
     UInt64_t key_;
 
 }; // namespace Diagrammatic
