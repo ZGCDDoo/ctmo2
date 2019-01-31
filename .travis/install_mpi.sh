9eeeaf3e46958e4968d501dc5f569556b800a444
charles-david <hebertcharlesdavid@outlook.com>
2019-01-31 16:20:41 -0500

    Corrected install_mpi.sh

--- .travis/install_mpi.sh
+++ .travis/install_mpi.sh
@@ -6,17 +6,13 @@ cd build_mpi
 cmake -DMPI_BUILD=ON .. 
 make -j 4 install
 sudo cp ctmo* /usr/bin/
+#!/bin/bash
 
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    
-else
+cd $TRAVIS_BUILD_DIR
+mkdir build_mpi
+cd build_mpi
+cmake -DMPI_BUILD=ON .. 
+make -j 4 install
+sudo cp ctmo* /usr/bin/
 
-    cd $TRAVIS_BUILD_DIR
-    mkdir build_mpi
-    cd build_mpi
-    cmake -DMPI_BUILD=ON .. 
-    make -j 4 install
-    sudo cp ctmo* /usr/bin/
-
-fi
