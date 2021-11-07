6e8aa348978ec8156d827e46cbe6fdce684d6153
charles-david hebert <hebertcharlesdavid@outlook.com>
2021-11-07 12:36:30 -0500

    moved to src and tests

--- NCtmo2/scripts/scaffold_legacy_db.ps1
+++ NCtmo2/scripts/scaffold_legacy_db.ps1
@@ -4,6 +4,14 @@
 [string] $userName = $credentials.UserName
 [string] $passWord = ConvertFrom-SecureString -SecureString $credentials.Password -AsPlainText
 
-Write-Host $userName
-Write-Host $passWord
+[string] $provider = "Npgsql.EntityFrameworkCore.PostgreSQL"
+[string] $connectionString = "Host=localhost;Database=ctmo2;Username=postgres;Password=$passWord"
+Write-Host $connectionString
+
+
+dotnet ef dbcontext scaffold $connectionString $provider              `
+        --context SimulationLegacyContext --verbose                   `
+        --startup-project "src/NCtmo2.API/NCtmo2.API.csproj"          `
+        --project "src/NCtmo2.Core/NCtmo2.Core.csproj"                `
+        --schema public --output-dir "src/NCtmo2.Core/Contexts/Legacy" 
 
